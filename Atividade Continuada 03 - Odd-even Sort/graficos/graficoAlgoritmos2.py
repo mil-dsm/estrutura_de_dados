4bcf66a7c5084835171d199341833d4196b3ca36
Milena <milenadsmesquita@alu.ufc.br>
2025-11-01 14:23:02 -0300

    Update graficoAlgoritmos2.py

--- Atividade Continuada 03 - Odd-even Sort/graficos/graficoAlgoritmos2.py
+++ Atividade Continuada 03 - Odd-even Sort/graficos/graficoAlgoritmos2.py
@@ -32,13 +32,6 @@ t_selection, tempo_selection = ler_dados(arquivo_selection)
 t_merge, tempo_merge = ler_dados(arquivo_merge)
 t_quick, tempo_quick = ler_dados(arquivo_quick)
 
-tempo_bubble   = [t * 1e6 for t in tempo_bubble]
-tempo_oddeven  = [t * 1e6 for t in tempo_oddeven]
-tempo_insertion = [t * 1e6 for t in tempo_insertion]
-tempo_selection = [t * 1e6 for t in tempo_selection]
-tempo_merge    = [t * 1e6 for t in tempo_merge]
-tempo_quick    = [t * 1e6 for t in tempo_quick]
-
 # Criar o gráfico
 plt.figure(figsize=(12,7))
 
@@ -110,6 +103,13 @@ plt.grid(True, linestyle='--', alpha=0.6)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.legend(fontsize=12)
+
+import matplotlib.ticker as mticker
+
+ax = plt.gca()
+ax.ticklabel_format(style='plain', axis='y', useOffset=False)
+ax.yaxis.set_major_formatter(mticker.StrMethodFormatter('{x:.0f}'))
+ax.get_yaxis().get_offset_text().set_visible(False)
 
 # Salvar e mostrar
 plt.tight_layout()
